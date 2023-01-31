de18c8473148e23c6f4c0f5ffd1e8dc5c8de0f8f
Kolya Voronov <RavenLebron@yandex.by>
2023-01-31 22:27:08 +0300

    feat:add function

--- counter/src/App.js
+++ counter/src/App.js
@@ -1,7 +1,31 @@
+import { useState } from "react";
 import "./App.css";
 
 function App() {
-  return <div className="App"></div>;
+  let [count, setCount] = useState(0);
+
+  const onClickMinus = () => {
+    setCount(count - 1);
+  };
+  const onClickPlus = () => {
+    setCount(count + 1);
+  };
+  return (
+    <div className="App">
+      <div>
+        <p>Счетчик:</p>
+        <p>
+          <strong>{count}</strong>
+        </p>
+        <button className="minus" onClick={onClickMinus}>
+          - Минус
+        </button>
+        <button className="plus" onClick={onClickPlus}>
+          Плюс +
+        </button>
+      </div>
+    </div>
+  );
 }
 
 export default App;
