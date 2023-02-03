7328dc63bcc9fb87782d34b6c55fff0c59f07660
Kolya Voronov <RavenLebron@yandex.by>
2023-02-03 21:18:26 +0300

    init: start modal

---
+++ modal/src/App.js
@@ -0,0 +1,14 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <button className="btn-modal">Нажми меня!</button>
+      <div className="overlay">
+        <div className="modal"></div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
